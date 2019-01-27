99e83ce7dac41e50f4c27c74f7addfe7d10e3e02
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2019-01-27 09:00:13 +0100

    V 0.1d (New apps)

---
+++ apps/ga.wjtje.content/app.js
@@ -0,0 +1,12 @@
+var ga_wjtje_content = new app('ga.wjtje.content', 'More content bro');
+
+ga_wjtje_content.menu('More content bro');
+
+ga_wjtje_content.create = function () {
+	return {
+		'name': 'More content bro',
+		'bg': '#0c0c5a',
+		'fg': '#fff',
+		'content': '<div class="OnlineOs-app-header" style="background-color: #10107a; color: #fff;">CONTENT!</div><div class="OnlineOs-app-content"><h1>We need more content</h1><p>AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz</p></div>'
+	};
+}
