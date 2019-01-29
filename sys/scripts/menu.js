fffdde19f501b8ab87202f0572cafd238ce3e99a
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2019-01-29 19:39:54 +0100

    Code removed

--- sys/scripts/menu.js
+++ sys/scripts/menu.js
@@ -24,19 +24,10 @@ function menu_reload() {
 			$($('.OnlineOs-menu-item')[i]).click(function () {
 				$this = $(this)[0];
 				$windows = $('.OnlineOs-window');
-				$found = false;
 				
-				//for (var j = 0; j < $windows.length; j++) {
-				//	if ($windows[j].dataset.appId == $this.dataset.app) {
-				//		$found = true;
-				//	}
-				//}
-				
-				if (!$found) {
-					// Create app
-					var $window_id = window_create($this.dataset.app);
-					taskbar_create($this.dataset.app, $window_id);
-				}
+				// Create app
+				var $window_id = window_create($this.dataset.app);
+				taskbar_create($this.dataset.app, $window_id);
 				
 				// Close menu
 				$('.OnlineOs-menu').css('display', 'none');
